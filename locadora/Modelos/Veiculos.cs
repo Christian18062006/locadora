d94a335e378a690bc2c708399b23bbb6c6550fac
carlos Christian <cursosacessiveis202108@gmail.com>
2024-10-12 18:42:31 -0300

    Criado o name space Modelos e a classe base Veiculos

---
+++ locadora/Modelos/Veiculos.cs
@@ -0,0 +1,14 @@
+﻿namespace locadora.Modelos;
+
+internal class Veiculos
+{
+    public String Marca { get; }
+    public int Ano { get; }
+    public String Modelo { get; }
+    public Veiculos(String marca, String modelo, int ano)
+    {
+        Marca = marca;
+        Modelo = modelo;
+        Ano = ano;
+    }
+}
