44d90941af313bd1f44504b9e99f3b58bb682ac4
carlos Christian <cursosacessiveis202108@gmail.com>
2024-10-12 11:44:30 -0300

    esqueleto do projeto com git ignore

---
+++ locadora/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace locadora;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem-vindo a locadora alugue rápido!");
+        Console.WriteLine("Aluguel de veículos com estilo!");
+    }
+}
