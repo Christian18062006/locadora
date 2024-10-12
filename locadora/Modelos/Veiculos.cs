ea3aaddd328afa700f7ca250afcf78e87fef00af
carlos Christian <cursosacessiveis202108@gmail.com>
2024-10-12 19:08:45 -0300

    Modificada a classe Veículos, adicionado o método exibir detalhes e a propriedade valor da locação

--- locadora/Modelos/Veiculos.cs
+++ locadora/Modelos/Veiculos.cs
@@ -5,10 +5,16 @@ internal class Veiculos
     public String Marca { get; }
     public int Ano { get; }
     public String Modelo { get; }
-    public Veiculos(String marca, String modelo, int ano)
+    public double ValorLocacao { get;}
+    public Veiculos(String marca, String modelo, int ano, double valorLocacao)
     {
         Marca = marca;
         Modelo = modelo;
         Ano = ano;
+        ValorLocacao = valorLocacao;
+    }
+    public virtual String ExibirDetalhes()
+    {
+        return ($"Marca: {Marca} modelo: {Modelo} ano de fabricação: {Ano} valor para locação: {ValorLocacao}");
     }
 }
