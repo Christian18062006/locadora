373eb6db8eebb67aa290278099bf15dc4e695d1a
carlos Christian <cursosacessiveis202108@gmail.com>
2024-10-12 19:25:37 -0300

    criada a classe Carro

---
+++ locadora/Modelos/Carro.cs
@@ -0,0 +1,16 @@
+﻿namespace locadora.Modelos
+{
+    internal class Carro:Veiculos
+    {
+        public int QuantidadePortas { get; }
+        public Carro(String marca, String modelo, int anoFabricacao, double valorLocacao, int quantidadePortas):base(marca,modelo,anoFabricacao,valorLocacao)
+        {
+QuantidadePortas = quantidadePortas;
+        }
+        public override string ExibirDetalhes()
+        {
+            String detalhes = base.ExibirDetalhes();
+            return $"{detalhes} Quantidade de portas: {QuantidadePortas}";
+        }
+    }
+}
